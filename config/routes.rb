41c538b7fafae0f10a685f038e944d21278dde4b
loezzje <m.demmendaal@hotmail.com>
2017-09-01 16:41:29 +0200

    create delete and edit methods

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
   get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
   post "dinosaurs" => "dinosaurs#create"
+  get "dinosaurs/:id/edit" => "dinosaurs#edit", as: :edit_dinosaur
+  patch "dinosaurs/:id" => "dinosaurs#update"
+  delete "dinosaurs/:id" => "dinosaurs#destroy"
 
 
 end
