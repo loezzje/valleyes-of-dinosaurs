dbf0b43890ea27150597c23fe9f3aac26bfbb663
loezzje <m.demmendaal@hotmail.com>
2017-09-01 16:17:42 +0200

    create new dinosaurs pages

---
+++ app/controllers/dinosaurs_controller.rb
@@ -0,0 +1,31 @@
+class DinosaursController < ApplicationController
+
+  def index
+    @dinosaurs = Dinosaur.all
+  end
+
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+      redirect_to @dinosaur
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def dinosaur_params
+    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :img_url)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
-dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" }
-)
+dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Aristratus", age: rand(1..100), image_url: "https://goo.gl/EqRlgP"})
+dino5 = Dinosaur.create!( { name: "Valeptrosaurus", age: rand(1..100), image_url: "https://goo.gl/EqRlgP"})
